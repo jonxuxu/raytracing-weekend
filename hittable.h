e4019c3e6a3effdc84c95a471b4e5bd2e3688c82
jonathan <jonathanxu25@gmail.com>
2021-06-24 10:09:24 -0600

    part 9

--- hittable.h
+++ hittable.h
@@ -1,12 +1,17 @@
 #ifndef HITTABLE_H
 #define HITTABLE_H
 
+#include "rtweekend.h"
+
 #include "ray.h"
+
+class material;
 
 struct hit_record
 {
     point3 p;
     vec3 normal;
+    shared_ptr<material> mat_ptr;
     double t;
     bool front_face;
 
--- vec3.h
+++ vec3.h
@@ -61,6 +61,13 @@ public:
         return vec3(random_double(min, max), random_double(min, max), random_double(min, max));
     }
 
+    bool near_zero() const
+    {
+        // Return true if the vector is close to zero in all dimensions.
+        const auto s = 1e-8;
+        return (fabs(e[0]) < s) && (fabs(e[1]) < s) && (fabs(e[2]) < s);
+    }
+
 public:
     double e[3];
 };
@@ -145,6 +152,11 @@ vec3 random_in_hemisphere(const vec3 &normal)
         return in_unit_sphere;
     else
         return -in_unit_sphere;
+}
+
+vec3 reflect(const vec3 &v, const vec3 &n)
+{
+    return v - 2 * dot(v, n) * n;
 }
 
 #endif
